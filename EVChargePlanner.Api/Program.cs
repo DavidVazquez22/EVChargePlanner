a5ca0a0267225e97cac71a5d1beae597c90fdf91
DavidVazquez22 <davidvazquezvazquez.22@gmail.com>
2026-08-08 16:58:45 +0200

    Pass VITE_API_URL to the Static Web Apps build

--- EVChargePlanner.Api/Program.cs
+++ EVChargePlanner.Api/Program.cs
@@ -51,7 +51,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "https://zealous-flower-034106c0f.7.azurestaticapps.net")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
